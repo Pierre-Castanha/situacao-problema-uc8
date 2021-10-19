b2d1386d59273c1472f508b31b2f2ea4a1735201
Pierre Castanha <adv.pierrecastanha@gmail.com>
2021-10-18 21:41:36 -0300

    Atualizando a UC 88

--- situacao-problema-uc8.js
+++ situacao-problema-uc8.js
@@ -1,4 +1,41 @@
-//  SISTEMA DE CADASTRO DE EVENTOS 
+                                             ///////////////////////////////////////////////////////
+                                             //              SENAI - Módulo : UC 8                //
+                                             //          Sistema de Cadastro de Eventos           //
+                                             //          Situação Problema - 18.10.2021           //
+                                             // Instrutores: Thiago Nascimento && Ordilei Sabella //
+                                             ///////////////////////////////////////////////////////
+
+
+/* /////////////////////////////////////////////////////// R E Q U I S I T O S ///////////////////////////////////////////////
+//                                                                                                                          //
+//   1.Você será responsável por escolher qual técnica irá atender os seguintes requisitos:                                 //
+//                                                                                                                          //
+//   -Se a data do evento for posterior à data atual, permitir o evento; senão, alertar que o cadastro não será permitido   //
+//     por data inválida.                                                                                                   //                                                                                                 //
+//                                                                                                                          //
+//   -Se o participante for maior de 18 anos, permitir o cadastro; senão, alertar que o cadastro não é permitido pela idade.//
+//                                                                                                                          //
+//   -Listar participantes e palestrantes por evento.                                                                       //
+//                                                                                                                          //
+//   -Enquanto a quantidade de participantes for inferior a 100, permitir cadastro; senão, alertar que o cadastro não será  //
+//    permitido por ter excedido o limite.                                                                                  //
+//                                                                                                                          //
+//   -Criar fluxograma com todos os requisitos que devem ser atendidos pelo sistema.                                        //
+//                                                                                                                          //
+//   Para criação desse sistema de cadastro, você, enquanto programador(a) responsável, deverá solucionar os seguintes      //
+//   desafios:                                                                                                              //
+//                                                                                                                          //
+//    - Descrever a sequência de passos lógicos necessários para criação do sistema solicitado pela empresa.                //
+//                                                                                                                          //
+//    -Aplicar técnicas de programação e ferramentas para desenvolvimento do código.                                        //
+//                                                                                                                          //
+//    -Aplicar linguagem de programação para implementação do sistema                                                       //
+//                                                                                                                          //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+
 
 let CadEvento = ["Aulão sobre JavaScript", "Paletra sobre Água Portável", "TI em tempo de Covid", "Onovo mundo Híbrido", "Programação em tempo de Covid"];
 let CadPalestrante = ["Professor João Albertto", "Gustavo Marins", "João Jorge", "Humberto Leal", "O Comendador"];
